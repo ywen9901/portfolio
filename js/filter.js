8d882b237ea57e411f4adfa713fd3f149e2f54ed
= <ywen9901@gmail.com>
2023-03-21 15:38:53 +0800

    Correct date in experience section

--- js/filter.js
+++
@@ -1,9 +0,0 @@
-function filterSelection(c) {
-    var x, i;
-    x = $(".filterDiv");
-    for(i=0;i<x.length;i++) {
-      x[i].style.display = 'none';
-      if(x[i].className.indexOf(c) > -1) x[i].style.display = 'block'; 
-    }
-  }
-  
